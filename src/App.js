637f53a451b3c22fa4eac962a3f07379e58951cc
VishalGcod <gvishal17102000@gmail.com>
2023-08-25 11:34:40 +0530

    Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Routers, Link, Route, Routes } from "react-router-dom";
 import CompOne from './components/compOne';
-import CompTwo from './components/compTwo';
+// import CompTwo from './components/compTwo';
 import EmptyPage from './components/emptyPage';
+import { LoginForm } from './components/loginPage';
 
 function App() {
   return (
@@ -10,11 +11,11 @@ function App() {
       <Routers>
         <div>
           <Link to='/'><span>Home</span></Link>
-          <Link to='/about'><span>About</span></Link>
+          <Link to='/login'><span>Login</span></Link>
         </div>
         <Routes>
         <Route path='/' element={<CompOne/>}></Route>
-        <Route path='/about' element={<CompTwo/>}></Route>
+        <Route path='/login' element={<LoginForm/>}></Route>
         <Route path='*' element={<EmptyPage/>}></Route>
         </Routes>
       </Routers>
